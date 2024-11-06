d3d40c5d8edcfe2a4f4df7064efd2138caa53121
Vaibhav Malhotra <115654765+vaibha-v19@users.noreply.github.com>
2024-11-06 16:48:13 +0530

    Update RecentProjects.tsx

--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -22,7 +22,7 @@ const RecentProjects = () => {
           >
             <PinContainer
               title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
